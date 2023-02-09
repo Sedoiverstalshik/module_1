bf481e4fd45e6a234bfdf8012ec0f149098c0c22
Николай Асташев <sedoiverstalshik@mail.ru>
2023-02-09 21:57:53 +0300

    добавлен первый товар

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict'; 
+
+{
+  const product = 'book';
+  const count = 5;
+  const category = 'history';
+  const price = 500;
+
+  console.log(product); //выводим наименование товара
+  console.log(count * price); //общая сумма товара
+}
